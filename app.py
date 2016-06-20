b42d99244cdbc318515f0b1a642b3b1e5ada8b39
Your Name <nwmullen@gmail.com>
2016-06-20 18:37:45 -0400

    trying pluralization of model name

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 
-class Customer(db.Model):
+class Customers(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     created_at = db.Column(db.DateTime)
@@ -34,7 +34,7 @@ def callback():
     oauth_web_auth_code_grant.request_oauth_tokens_by_response_uri(request.url)
     oauth_tokens = oauth_web_auth_code_grant.oauth_tokens
     access_token = oauth_tokens.access_token
-    customer = Customer.query.get(session.pop('customer_id', None))
+    customer = Customers.query.get(session.pop('customer_id', None))
     customer.bing_ads_api_key = access_token
     db.session.commit()
     return "Success? Maybe..."
