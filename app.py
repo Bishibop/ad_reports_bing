ae28020dbd817a5f106b1bd43dd0763b77ea9d1c
Your Name <nwmullen@gmail.com>
2016-06-23 17:06:33 -0400

    bugfix-adds in dbcolumn for expires in which MISTERIOUSLY DISAPPEARED

--- app.py
+++ app.py
@@ -12,13 +12,14 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 
 class Customers(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    created_at = db.Column(db.DateTime)
-    updated_at = db.Column(db.DateTime, onupdate=datetime.now)
-    bing_ads_access_token = db.Column(db.String)
-    bing_ads_refresh_token = db.Column(db.String)
-    bing_ads_issued_at = db.Column(db.DateTime)
+    id =                            db.Column(db.Integer, primary_key=True)
+    name =                          db.Column(db.String)
+    created_at =                    db.Column(db.DateTime)
+    updated_at =                    db.Column(db.DateTime, onupdate=datetime.now)
+    bing_ads_access_token =         db.Column(db.String)
+    bing_ads_refresh_token =        db.Column(db.String)
+    bing_ads_issued_at =            db.Column(db.DateTime)
+    bing_ads_expires_in_seconds =   db.Column(db.Integer)
 
 @app.route("/<customer_id>")
 def register(customer_id):
