21be11894a9851b6d131f62d1073d1e7034cc7ea
Your Name <nwmullen@gmail.com>
2016-07-10 17:34:12 -0400

    testing alternative setup for shelltools

---
+++ shelltools.py
@@ -0,0 +1,9 @@
+
+def test_shell_tools():
+    print('testing!!!!!!!')
+# def get_bing_reports():
+
+
+# @app.cli.command()
+# def test_shell_function():
+    # click.echo('testing the shell functionality')
